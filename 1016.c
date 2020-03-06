3186fad7f4dcbc59d82b52fde01b6fca1208d996
Eduardo <eduardokasaki@hotmail.com>
2020-03-06 00:38:36 -0300

    Uri1016

---
+++ 1016.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+int tempo =2, y , distancia ;
+
+scanf("%d", &y);
+distancia = y * tempo;
+printf("%d minutos\n", distancia);
+
+
+return 0;
+
+
+  }
